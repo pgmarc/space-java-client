c30717ec36d66d6986f245a6a5de3ccaea15fad8
Pedro González Marcos <pedgonmar2@alum.us.es>
2025-08-13 13:02:09 +0200

    build(pom): Maven POM initial setup
    Configure several plugins in build

---
+++ src/main/java/io/github/pgmarc/space/App.java
@@ -0,0 +1,7 @@
+package io.github.pgmarc.space;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/io/github/pgmarc/space/AppTest.java
@@ -0,0 +1,13 @@
+package io.github.pgmarc.space;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
