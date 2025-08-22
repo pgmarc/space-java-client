420617f8c689e9ae50a152a217ab410547b2f322
Pedro González Marcos <pedgonmar2@alum.us.es>
2025-08-22 21:57:17 +0200

    feat: migrate MockServer to WireMock

--- src/test/java/io/github/pgmarc/space/deserializers/SubscriptionSerializerTest.java
+++ src/test/java/io/github/pgmarc/space/deserializers/SubscriptionSerializerTest.java
@@ -2,11 +2,9 @@ package io.github.pgmarc.space.deserializers;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -20,15 +18,58 @@ class SubscriptionSerializerTest {
     @Test
     void givenSubscriptionAsJsonShouldCreateSubscription() {
 
-        String content = null;
-        try {
-            byte[] raw = Files.readAllBytes(Paths.get("src", "test", "resources", "subscription-response.json"));
-            content = new String(raw);
-        } catch (IOException e) {
-            fail(e.getCause());
-        }
+        JSONObject input = new JSONObject(Map.of(
+                "id", "68050bd09890322c57842f6f",
+                "userContact", Map.of(
+                        "userId", "01c36d29-0d6a-4b41-83e9-8c6d9310c508",
+                        "username", "johndoe",
+                        "fistName", "John",
+                        "lastName", "Doe",
+                        "email", "john.doe@my-domain.com",
+                        "phone", "+34 666 666 666"),
+                "billingPeriod", Map.of(
+                        "startDate", "2025-12-31T00:00:00Z",
+                        "endDate", "2025-12-31T00:00:00Z",
+                        "autoRenew", true,
+                        "renewalDays", 365),
+                "usageLevel", Map.of(
+                        "zoom", Map.of(
+                                "maxSeats", Map.of("consumed", 10)),
+                        "petclinic", Map.of(
+                                "maxPets", Map.of("consumed", 2),
+                                "maxVisits", Map.of(
+                                        "consumed", 5,
+                                        "resetTimeStamp", "2025-07-31T00:00:00Z"))),
+                "contractedServices", Map.of(
+                        "zoom", "2025",
+                        "petclinic", "2024"),
+                "subscriptionPlans", Map.of(
+                        "zoom", "ENTERPRISE",
+                        "petclinic", "GOLD"),
+                "subscriptionAddOns", Map.of(
+                        "zoom", Map.of(
+                                "extraSeats", 2,
+                                "hugeMeetings", 1),
+                        "petclinic", Map.of(
+                                "petsAdoptionCentre", 1)),
+                "history", List.of(
+                        Map.of(
+                                "startDate", "2025-12-31T00:00:00Z",
+                                "endDate", "2025-12-31T00:00:00Z",
+                                "contractedServices", Map.of(
+                                        "zoom", "2025",
+                                        "petclinic", "2024"),
+                                "subscriptionPlans", Map.of(
+                                        "zoom", "ENTERPRISE",
+                                        "petclinic", "GOLD"),
+                                "subscriptionAddOns", Map.of(
+                                        "zoom", Map.of(
+                                                "extraSeats", 2,
+                                                "hugeMeetings", 1),
+                                        "petclinic", Map.of(
+                                                "petsAdoptionCentre", 1))))));
 
-        Subscription actual = serializer.fromJson(new JSONObject(content));
+        Subscription actual = serializer.fromJson(input);
         assertAll(
                 () -> assertEquals(1, actual.getHistory().size()),
                 () -> assertEquals(2, actual.getUsageLevels().size()),
