be193d832b14b4a96da4839d585e034c819f93db
Pedro González Marcos <pedgonmar2@alum.us.es>
2025-08-31 11:32:07 +0200

    feat: allow usage level with zero consumption

--- space-client/src/test/java/io/github/pgmarc/space/contracts/UsageLevelTest.java
+++ space-client/src/test/java/io/github/pgmarc/space/contracts/UsageLevelTest.java
@@ -1,14 +1,13 @@
 package io.github.pgmarc.space.contracts;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 
 class UsageLevelTest {
 
@@ -16,39 +15,38 @@ class UsageLevelTest {
     void givenNonRenewableUsageLimitShouldCreate() {
 
         String usageLimitName = "maxPets";
-        double consumption = 5;
+        double consumption = 0;
         UsageLevel usageLevel = UsageLevel.of(usageLimitName, consumption);
 
         assertAll(
-                () -> assertEquals(usageLimitName, usageLevel.getName()),
-                () -> assertEquals(consumption, usageLevel.getConsumption()),
-                () -> assertFalse(usageLevel.isRenewableUsageLimit()));
+                () -> assertThat(usageLevel.getName()).isEqualTo(usageLimitName),
+                () -> assertThat(usageLevel.getConsumption()).isEqualTo(consumption),
+                () -> assertThat(usageLevel.isRenewableUsageLimit()).isFalse());
     }
 
     @Test
-    void givenInvalidParamertersShouldThrow() {
+    void givenNullUsageLimitNameShouldThrow() {
 
-        String usageLimitName = "maxPets";
-        double consumption = 5;
+        assertThatExceptionOfType(NullPointerException.class)
+            .isThrownBy(() -> UsageLevel.of(null, 5))
+                .withMessage("usage limit name must not be null");
+    }
 
-        assertAll(
-                () -> assertThrows(NullPointerException.class, () -> UsageLevel.of(null, consumption)),
-                () -> assertThrows(IllegalArgumentException.class, () -> UsageLevel.of(usageLimitName, -1)));
+    @Test
+    void givenNegativeConsumptionShouldThrow() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> UsageLevel.of("maxPets", -1))
+            .withMessage("usage level consumption must be positive");
     }
 
     @Test
     void givenRenewableUsageLimitShouldCreate() {
 
-        String usageLimitName = "maxTokens";
-        double consumption = 300;
         ZonedDateTime resetTimestamp = ZonedDateTime.parse("2025-08-19T00:00:00Z");
-        UsageLevel usageLevel = UsageLevel.of(usageLimitName, consumption, resetTimestamp);
+        UsageLevel usageLevel = UsageLevel.of("maxTokens", 300, resetTimestamp);
 
-        assertAll(
-                () -> assertEquals(usageLimitName, usageLevel.getName()),
-                () -> assertEquals(consumption, usageLevel.getConsumption()),
-                () -> assertEquals(resetTimestamp.toLocalDateTime(), usageLevel.getResetTimestamp().get()),
-                () -> assertTrue(usageLevel.isRenewableUsageLimit()));
+        assertThat(usageLevel.getResetTimestamp()).hasValue(resetTimestamp.toLocalDateTime());
+        assertThat(usageLevel.isRenewableUsageLimit()).isTrue();
     }
 
 }
